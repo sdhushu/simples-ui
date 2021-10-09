35cbca0cd05f0adc65f31eaea7fe8c402f76b600
sdhushu <54340686+sdhushu@users.noreply.github.com>
2021-10-09 09:06:16 +0000

    Deploying to gh-page from @ sdhushu/simples-ui@f3baea5b3cc1f4176c4fe114b50aa758e8987473 🚀

--- assets/index.da85bcde.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./TabItem.2579711c.js";import{_ as t}from"./preload-helper.0a5f1423.js";import{q as a,d as r,o as m,k as s}from"./vendor.2c8e45dd.js";e.install=function(o){o.component(e.name,e)};const c=a({name:"TabItemExample",components:{[e.name]:e}});function p(o,_,i,d,f,l){const n=r("var-tab-item");return m(),s(n)}var x=t(c,[["render",p]]);export{x as default};
